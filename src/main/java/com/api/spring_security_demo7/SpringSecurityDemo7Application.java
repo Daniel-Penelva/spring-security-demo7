be91ad526086c14ed6f54764f544c9b8e9829ea0
Daniel Penelva <d4n.andrade@gmail.com>
2025-08-23 00:09:08 -0300

    feat: criando projeto Spring Boot e Spring Security

---
+++ src/main/java/com/api/spring_security_demo7/SpringSecurityDemo7Application.java
@@ -0,0 +1,13 @@
+package com.api.spring_security_demo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/spring_security_demo7/SpringSecurityDemo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.spring_security_demo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
