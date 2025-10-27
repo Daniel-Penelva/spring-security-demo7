c0d27a8b554a11f9999b23513fd76a9a423d2d74
Daniel Penelva <d4n.andrade@gmail.com>
2025-10-27 19:26:08 -0300

    feat: (funcionalidade: criando um enum response para gerar o id) criando RestResponse

---
+++ src/main/java/com/api/spring_security_demo7/common/RestResponse.java
@@ -0,0 +1,16 @@
+package com.api.spring_security_demo7.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RestResponse {
+    private String id;
+}
